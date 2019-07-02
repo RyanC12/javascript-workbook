caa93a23992121f2311565cce0b2fa882920af8b
unknown <rmi11s@yahoo.com>
2019-07-02 00:55:04 -0500

    final

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -21,175 +21,134 @@ function printBoard() {
 
 function horizontalWin() {
 	// Your code here
+	// checks for all horizontal win conditions
 	for (let i = 0; i <= board.length; i++) {
-		if (board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") {
-			console.log("X wins");
-		}
-		if (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") {
-			console.log("X wins");
-		}
-		if (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") {
-			console.log("X wins");
-		} else if (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") {
-			console.log("O wins");
-		}
-		if (board[1][0] == "O" && board[2][1] == "O" && board[1][2] == "O") {
-			console.log("O wins");
-		}
-		if (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O") {
-			console.log("O wins");
+		if (
+			board[0][0] == playerTurn &&
+			board[0][1] == playerTurn &&
+			board[0][2] == playerTurn
+		) {
+			console.log(playerTurn + " wins");
+			return true;
+		} else if (
+			board[1][0] == playerTurn &&
+			board[1][1] == playerTurn &&
+			board[1][2] == playerTurn
+		) {
+			console.log(playerTurn + " wins");
+			return true;
+		} else if (
+			board[2][0] == playerTurn &&
+			board[2][1] == playerTurn &&
+			board[2][2] == playerTurn
+		) {
+			console.log(playerTurn + " wins");
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
 
 function verticalWin() {
 	// Your code here
+	// checks for all vertical win conditions
+
 	for (let i = 0; i <= board.length; i++) {
-		if (board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") {
-			console.log("X wins");
-		}
-		if (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") {
-			console.log("X wins");
-		}
-		if (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") {
-			console.log("X wins");
-		} else if (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O") {
-			console.log("O wins");
-		}
-		if (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O") {
-			console.log("O wins");
-		}
-		if (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O") {
-			console.log("O wins");
+		if (
+			board[0][0] == playerTurn &&
+			board[1][0] == playerTurn &&
+			board[2][0] == playerTurn
+		) {
+			console.log(playerTurn + " wins");
+			return true;
+		} else if (
+			board[0][1] == playerTurn &&
+			board[1][1] == playerTurn &&
+			board[2][1] == playerTurn
+		) {
+			console.log(playerTurn + " wins");
+			return true;
+		} else if (
+			board[0][2] == playerTurn &&
+			board[1][2] == playerTurn &&
+			board[2][2] == playerTurn
+		) {
+			console.log(playerTurn + " wins");
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
 
 function diagonalWin() {
 	// Your code here
+	// checks for all diagonal win conditions
+
 	for (let i = 0; i <= board.length; i++) {
-		if (board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") {
-			console.log("X wins");
-		}
-		if (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") {
-			console.log("X wins");
-		} else if (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O") {
-			console.log("O wins");
-		}
-		if (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O") {
-			console.log("O wins");
+		if (
+			board[0][0] == playerTurn &&
+			board[1][1] == playerTurn &&
+			board[2][2] == playerTurn
+		) {
+			console.log(playerTurn + " wins");
+			return true;
+		} else if (
+			board[0][2] == playerTurn &&
+			board[1][1] == playerTurn &&
+			board[2][0] == playerTurn
+		) {
+			console.log(playerTurn + " wins");
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
 
 function checkForWin() {
-	// horizontalWin();
-	// verticalWin();
-	// diagonalWin();
+	// check to see if win conditions are true, if conditions are true reset the board
 	if (horizontalWin() == true) {
 		console.log("horizontal wins");
+		resetBoard();
+		return true;
 	} else if (verticalWin() == true) {
 		console.log("vertical wins");
+		resetBoard();
+		return true;
 	} else if (diagonalWin() == true) {
 		console.log("diagonal wins");
+		resetBoard();
+		return true;
 	} else {
-		getPrompt();
+		return false;
 	}
-	// Your code here
-	//horizontal, vert, diag
 }
-// function marker(row, column) {
-// 	if (row == 0 && column == 0) {
-// 		board[0][0] = playerTurn;
-// 		return;
-// 	}
-// }
-// function switchPlayer() {
-// 	if (playerTurn === "X") {
-// 		playerTurn = "O";
-// 	} else {
-// 		playerTurn = "X";
-// 	}
-// }
 
-function isSpotValid() {
-	// check if slot is available
-	if (board[0][0] == "X" || "O") {
-	}
-	if (board[0][1] == "X" || "O") {
-	}
-	if (board[0][2] == "X" || "O") {
-	}
-	if (board[1][0] == "X" || "O") {
-	}
-	if (board[1][1] == "X" || "O") {
-	}
-	if (board[1][2] == "X" || "O") {
-	}
-	if (board[2][0] == "X" || "O") {
-	}
-	if (board[2][1] == "X" || "O") {
-	}
-	if (board[2][2] == "X" || "O") {
-	}
+function resetBoard() {
+	// resets board back to original state
+	board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
 }
 
 function ticTacToe(row, column) {
 	// Your code here
-	// switchplayer, validmove,invalid
-	//order
-	// is valid(place x or o)
-	// check for win
-	// switch player
 
-	checkForWin();
-
-	if (row == 0 && column == 0) {
-		board[0][0] = playerTurn;
-	}
-	if (row == 0 && column == 1) {
-		board[0][1] = playerTurn;
-	}
-	if (row == 0 && column == 2) {
-		board[0][2] = playerTurn;
-	}
-	if (row == 1 && column == 0) {
-		board[1][0] = playerTurn;
-	}
-	if (row == 1 && column == 1) {
-		board[1][1] = playerTurn;
-	}
-	if (row == 1 && column == 2) {
-		board[1][2] = playerTurn;
-	}
-	if (row == 2 && column == 0) {
-		board[2][0] = playerTurn;
-	}
-	if (row == 2 && column == 1) {
-		board[2][1] = playerTurn;
-	}
-	if (row == 2 && column == 2) {
-		board[2][2] = playerTurn;
-	}
-	if (playerTurn === "X") {
-		playerTurn = "O";
+	// Checks if the row and column the user is selected are free or not
+	// if spot is taken have user select the row and column again
+	// alternate between X and O
+	if (board[row][column] === "X" || board[row][column] === "O") {
+		console.log("that spot is taken");
 	} else {
-		playerTurn = "X";
-	}
-	if (checkForWin() == true) {
-		return;
+		board[row][column] = playerTurn;
+		checkForWin();
+		if (playerTurn === "X") {
+			playerTurn = "O";
+		} else {
+			playerTurn = "X";
+		}
 	}
 }
-
-// function isNotValid() {
-// 	// if (board[][] == "X" || board[][] == "O")
-// }
-
-// isValidSpace() is the move allowed
-// inNotValid()
-// if(!board[1][1])
-// if(board[1][1])
-// if(current ==)
 
 function getPrompt() {
 	printBoard();
